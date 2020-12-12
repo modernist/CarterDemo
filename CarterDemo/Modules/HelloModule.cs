d9e8a804b17182760990a88512efdba2310adc0f
Kostas Stroggylos <modernist@users.noreply.github.com>
2020-12-12 03:13:04 +0200

    Added first module

---
+++ CarterDemo/Modules/HelloModule.cs
@@ -0,0 +1,41 @@
+﻿using Carter;
+using Carter.Request;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CarterDemo.Modules
+{
+    public class HelloModule : CarterModule
+    {
+        public HelloModule()
+        {
+            Get("/api/hello", (req, res) =>
+            {
+                return res.WriteAsync("Hello from Carter");
+            });
+
+            Get("/api/hello/add/{a:int}/{b:int}", (req, res) =>
+            {
+                var a = req.RouteValues.As<int>("a");
+                var b = req.RouteValues.As<int>("b");
+
+                return res.WriteAsync($"{a} + {b} = {a + b}");
+            });
+
+            Post("/api/hello/add", (req, res) =>
+            {
+                var a = int.Parse(req.Form["a"].FirstOrDefault());
+                var b = int.Parse(req.Form["b"].FirstOrDefault());
+                return res.WriteAsync($"{a} + {b} = {a + b}");
+            });
+
+            Delete("/api/hello", (ctx) =>
+            {
+                ctx.Response.StatusCode = (int)HttpStatusCode.NotImplemented;
+                return Task.CompletedTask;
+            });
+        }
+    }
+}
--- CarterDemo/Startup.cs
+++ CarterDemo/Startup.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Carter;
 
 namespace CarterDemo
 {
@@ -16,6 +17,7 @@ namespace CarterDemo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCarter();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +32,7 @@ namespace CarterDemo
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapCarter();
             });
         }
     }
