688413f69d43a561a169c92d561ba3f696839bd0
Kostas Stroggylos <modernist@users.noreply.github.com>
2020-12-12 03:32:17 +0200

    Added hooks module

---
+++ CarterDemo/Modules/HooksModule.cs
@@ -0,0 +1,31 @@
+﻿using Carter;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Net;
+
+namespace CarterDemo.Modules
+{
+    public class HooksModule : CarterModule
+    {
+        public HooksModule() : base("/api/hooks")
+        {
+            this.Before = async (context) =>
+            {
+                if (!context.Request.Headers.TryGetValue("x-custom-header", out var values) ||
+                    !values.Contains("let-me-in", StringComparer.InvariantCulture))
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                    await context.Response.WriteAsync("Required header missing");
+                    return false;
+                }
+
+                return true;
+            };
+
+            Get("/", (req, res) => res.WriteAsync("Got here"));
+
+            this.After = context => context.Response.WriteAsync("Ended up here");
+        }
+    }
+}
