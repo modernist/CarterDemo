c06d5f8abf6d95b149f6f4d802ffc8c73152e9bb
Kostas Stroggylos <modernist@users.noreply.github.com>
2020-12-12 03:29:36 +0200

    Model binding and validation

---
+++ CarterDemo/Models/ToDoItemValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace CarterDemo.Models
+{
+    public class ToDoItemValidator : AbstractValidator<ToDoItem>
+    {
+        public ToDoItemValidator()
+        {
+            RuleFor(item => item.Category).NotEmpty();
+            RuleFor(item => item.Description).NotEmpty();
+        }
+    }
+}
--- CarterDemo/Modules/ToDoModule.cs
+++ CarterDemo/Modules/ToDoModule.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
-using Carter;
+﻿using Carter;
+using Carter.ModelBinding;
 using Carter.Request;
 using Carter.Response;
 using CarterDemo.Models;
 using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace CarterDemo.Modules
 {
@@ -40,6 +39,69 @@ namespace CarterDemo.Modules
                 var items = provider.GetItems(item => string.Equals(item.Category, category, StringComparison.InvariantCultureIgnoreCase));
 
                 return res.Negotiate(items);
+            });
+
+            Post("/", async (req, res) =>
+            {
+                var request = await req.BindAndValidate<ToDoItem>();
+
+                if (!request.ValidationResult.IsValid)
+                {
+                    res.StatusCode = 400;
+                    await res.Negotiate(request.ValidationResult.GetFormattedErrors());
+                    return;
+                }
+
+                var item = request.Data;
+                var result = provider.AddItem(item);
+
+                if (!result)
+                {
+                    res.StatusCode = (int)HttpStatusCode.UnprocessableEntity;
+                    return;
+                }
+
+                res.StatusCode = (int)HttpStatusCode.Created;
+                await res.Negotiate(item);
+            });
+
+            Put("/{id:int}", async (req, res) =>
+            {
+                var (validationResult, item) = await req.BindAndValidate<ToDoItem>();
+
+                if (!validationResult.IsValid)
+                {
+                    res.StatusCode = 400;
+                    await res.Negotiate(validationResult.GetFormattedErrors());
+                    return;
+                }
+
+                var id = req.RouteValues.As<int>("id");
+                var (success, updatedItem) = provider.UpdateItem(id, item);
+
+                if (!success)
+                {
+                    res.StatusCode = (int)HttpStatusCode.UnprocessableEntity;
+                    return;
+                }
+
+                res.StatusCode = (int)HttpStatusCode.OK;
+                await res.Negotiate(updatedItem);
+            });
+
+            Delete("/{id:int}", (req, res) =>
+            {
+                var id = req.RouteValues.As<int>("id");
+
+                if (!provider.DeleteItem(id))
+                {
+                    res.StatusCode = (int)HttpStatusCode.NotFound;
+                }
+                else
+                {
+                    res.StatusCode = (int)HttpStatusCode.NoContent;
+                }
+                return Task.CompletedTask;
             });
         }
     }
